50d874782b641ea7fd736f5f56542dec1f4b5015
liaozhai <laocan@yandex.ru>
2026-05-25 02:08:08 +0300

    fix

--- crawler.go
+++ crawler.go
@@ -9,25 +9,30 @@ import (
 // K - key type, V - value type
 type Interface[K comparable, V any] interface {
 	Value() V
-	Nodes() []K
+	Keys() []K
 }
 
 type Transformer[K comparable, V any] func(t K) Interface[K, V]
 
-func run[K comparable, V any](seed K, depth int, transform Transformer[K, V], st *set.Set[K], wg *sync.WaitGroup, out chan V) {
-	t := transform(seed)
+type Result[K comparable, V any] struct {
+	Key   K
+	Value V
+}
+
+func run[K comparable, V any](key K, depth int, transform Transformer[K, V], st *set.Set[K], wg *sync.WaitGroup, out chan Result[K, V]) {
+	t := transform(key)
 	defer wg.Done()
-	for _, n := range t.Nodes() {
+	for _, n := range t.Keys() {
 		wg.Add(1)
 		go run(n, depth-1, transform, st, wg, out)
 	}
-	out <- t.Value()
+	out <- Result[K, V]{key, t.Value()}
 }
 
-func Crawl[K comparable, V any](seed K, depth int, transform Transformer[K, V]) chan V {
+func Crawl[K comparable, V any](seed K, depth int, transform Transformer[K, V]) chan Result[K, V] {
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
-	out := make(chan V)
+	out := make(chan Result[K, V])
 	go run(seed, depth, transform, set.New[K](), wg, out)
 	go func() {
 		wg.Wait()
