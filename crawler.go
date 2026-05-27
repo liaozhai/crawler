4e6d4bb17dce101c9364ee4dab68c8c2ee33f633
liaozhai <laocan@yandex.ru>
2026-05-27 15:14:25 +0300

    fix cache

--- crawler.go
+++ crawler.go
@@ -17,27 +17,40 @@ type Result[K comparable, V any] struct {
 	Value V
 }
 
-func run[K comparable, V any](key K, depth int, transform Transformer[K, V], st *sync.Map, wg *sync.WaitGroup, out chan Result[K, V]) {
+type locker[K comparable] struct {
+	mu    sync.Mutex
+	cache map[K]struct{}
+}
+
+func newLocker[K comparable]() *locker[K] {
+	return &locker[K]{sync.Mutex{}, make(map[K]struct{})}
+}
+
+func run[K comparable, V any](key K, depth int, transform Transformer[K, V], lck *locker[K], wg *sync.WaitGroup, out chan Result[K, V]) {
 	defer wg.Done()
-	if _, ok := st.Load(key); ok {
+	lck.mu.Lock()
+	if _, ok := lck.cache[key]; ok {
+		lck.mu.Unlock()
 		return
 	}
 	t, err := transform(key)
 	if err != nil {
+		lck.mu.Unlock()
 		return
 	}
-	st.Store(key, struct{}{})
+	lck.cache[key] = struct{}{}
 	out <- Result[K, V]{key, t.Value()}
+	lck.mu.Unlock()
 	for _, n := range t.Keys() {
 		wg.Add(1)
-		go run(n, depth-1, transform, st, wg, out)
+		go run(n, depth-1, transform, lck, wg, out)
 	}
 }
 
 func Crawl[K comparable, V any](seed K, depth int, transform Transformer[K, V], out chan Result[K, V]) {
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
-	go run(seed, depth, transform, &sync.Map{}, wg, out)
+	go run(seed, depth, transform, newLocker[K](), wg, out)
 	go func() {
 		wg.Wait()
 		close(out)
